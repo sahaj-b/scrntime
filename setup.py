8784a3dea26a80532f3d21db5c92a383dd92157b
sahaj-b <sahajb0606@gmail.com>
2025-02-21 20:34:20 +0530

    chore: update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="scrntime",
-    version="1.2.0",
+    version="1.4.0",
     py_modules=["scrntime"],
     entry_points={
         "console_scripts": [
